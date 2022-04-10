e367069e5cd5aea823e68a2616cf4bb31dee983e
98ping <67809373+98ping@users.noreply.github.com>
2022-04-10 15:45:30 -0700

    add punishment kick msgs

--- commons/src/main/kotlin/ltd/matrixstudios/alchemist/models/profile/GameProfile.kt
+++ commons/src/main/kotlin/ltd/matrixstudios/alchemist/models/profile/GameProfile.kt
@@ -1,7 +1,10 @@
 package ltd.matrixstudios.alchemist.models.profile
 
 import com.google.gson.JsonObject
+import ltd.matrixstudios.alchemist.models.grant.types.Punishment
 import ltd.matrixstudios.alchemist.models.ranks.Rank
+import ltd.matrixstudios.alchemist.punishments.PunishmentType
+import ltd.matrixstudios.alchemist.service.expirable.PunishmentService
 import ltd.matrixstudios.alchemist.service.expirable.RankGrantService
 import ltd.matrixstudios.alchemist.service.ranks.RankService
 import java.util.*
@@ -13,6 +16,14 @@ data class GameProfile(
     var usedIps: ArrayList<String>,
 ) {
 
+    fun getPunishments() : Collection<Punishment> {
+        return PunishmentService.getValues().filter { it.target == uuid }
+    }
+
+
+    fun hasActivePunishment(type: PunishmentType) : Boolean {
+        return getPunishments().find { it.getGrantable() == type } != null
+    }
 
     fun getCurrentRank(): Rank? {
         var currentGrant: Rank? = RankService.findFirstAvailableDefaultRank()
--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/listeners/profile/ProfileJoinListener.kt
+++ spigot/src/main/kotlin/ltd/matrixstudios/alchemist/listeners/profile/ProfileJoinListener.kt
@@ -3,17 +3,23 @@ package ltd.matrixstudios.alchemist.listeners.profile
 import com.google.common.base.Stopwatch
 import com.google.gson.JsonObject
 import ltd.matrixstudios.alchemist.models.profile.GameProfile
+import ltd.matrixstudios.alchemist.punishments.PunishmentType
 import ltd.matrixstudios.alchemist.service.profiles.ProfileGameService
+import ltd.matrixstudios.alchemist.util.Chat
 import ltd.matrixstudios.alchemist.util.SHA
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
+import sun.java2d.cmm.Profile
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 
 class ProfileJoinListener : Listener {
+
 
     @EventHandler
     fun join(event: AsyncPlayerPreLoginEvent) {
@@ -26,6 +32,23 @@ class ProfileJoinListener : Listener {
 
             println("Profile creation for " + event.name + " took " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + "ms")
 
+        }
+
+        val profile = ProfileGameService.byId(event.uniqueId)
+
+        if (profile == null) {
+            event.loginResult = AsyncPlayerPreLoginEvent.Result.KICK_OTHER
+            event.kickMessage = Chat.format("&cYour profile failed to load")
+
+            return
+        }
+
+        if (profile.hasActivePunishment(PunishmentType.BAN)) {
+            event.loginResult = AsyncPlayerPreLoginEvent.Result.KICK_BANNED
+            event.kickMessage = Chat.format("&cYou are currently banned from the server")
+        } else if (profile.hasActivePunishment(PunishmentType.BLACKLIST)) {
+            event.loginResult = AsyncPlayerPreLoginEvent.Result.KICK_BANNED
+            event.kickMessage = Chat.format("&cYou are currently blacklisted from the server")
         }
     }
 }
