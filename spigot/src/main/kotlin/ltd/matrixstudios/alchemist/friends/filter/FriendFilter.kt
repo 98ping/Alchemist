22180f764171fd565c59d4d9b7a143b8041095f1
Subham <growlyxdev@gmail.com>
2023-07-27 23:23:46 -0400

    chore: remove the terrible code

--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/friends/filter/FriendFilter.kt
+++
@@ -1,7 +0,0 @@
-package ltd.matrixstudios.alchemist.friends.filter
-
-enum class FriendFilter(val displayName: String)
-{
-    ALL("All"), ONLINE("Online Friends"), OFFLINE("Offline Friends"), YOUR_SERVER("Your Server"), RECENTLY_JOINED("Recently Joined");
-
-}
