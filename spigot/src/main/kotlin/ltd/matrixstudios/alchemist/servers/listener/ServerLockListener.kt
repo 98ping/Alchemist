a5cbdaa4944b82a157a69bbe3b4dd890097e4169
Max <67809373+98ping@users.noreply.github.com>
2023-08-20 22:10:22 -0700

    chore: audit message when players join

--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/servers/listener/ServerLockListener.kt
+++ spigot/src/main/kotlin/ltd/matrixstudios/alchemist/servers/listener/ServerLockListener.kt
@@ -2,11 +2,15 @@ package ltd.matrixstudios.alchemist.servers.listener
 
 import ltd.matrixstudios.alchemist.Alchemist
 import ltd.matrixstudios.alchemist.api.AlchemistAPI
+import ltd.matrixstudios.alchemist.packets.OwnershipMessagePacket
+import ltd.matrixstudios.alchemist.packets.StaffAuditPacket
+import ltd.matrixstudios.alchemist.redis.AsynchronousRedisSender
 import ltd.matrixstudios.alchemist.service.ranks.RankService
 import ltd.matrixstudios.alchemist.util.Chat
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
+import java.util.concurrent.TimeUnit
 
 class ServerLockListener : Listener {
 
@@ -24,6 +28,7 @@ class ServerLockListener : Listener {
             if (theirRank == null || theirRank.weight < rank.weight) {
                 event.loginResult = AsyncPlayerPreLoginEvent.Result.KICK_OTHER
                 event.kickMessage = Chat.format("&cThis server is currently locked.\nTo bypass, you must have ${rank.color}${rank.displayName}&c!")
+                AsynchronousRedisSender.send(StaffAuditPacket(Chat.format("&b[S] &3[${server.displayName}] &r${theirRank.color + it.username} &3tried joining with a rank priority less than that of ${rank.color + rank.displayName}&3.")))
             }
         }
     }
