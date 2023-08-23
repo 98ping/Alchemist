7d08d405d01135f7b7412d2183d3e49fa7672b94
Preceding <49699421+PrecedingxD@users.noreply.github.com>
2023-08-23 08:20:51 -0500

    Fix: Add rank colored usernames for rank grant menu

--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/grants/menu/grant/GrantMenu.kt
+++ spigot/src/main/kotlin/ltd/matrixstudios/alchemist/grants/menu/grant/GrantMenu.kt
@@ -2,6 +2,7 @@ package ltd.matrixstudios.alchemist.grants.menu.grant
 
 import ltd.matrixstudios.alchemist.models.profile.GameProfile
 import ltd.matrixstudios.alchemist.service.ranks.RankService
+import ltd.matrixstudios.alchemist.util.Chat
 import ltd.matrixstudios.alchemist.util.menu.Button
 import ltd.matrixstudios.alchemist.util.menu.pagination.PaginatedMenu
 import ltd.matrixstudios.alchemist.util.menu.type.BorderedPaginatedMenu
@@ -21,6 +22,6 @@ class GrantMenu(val player: Player, val gameProfile: GameProfile) : BorderedPagi
     }
 
     override fun getTitle(player: Player): String {
-        return "Granting for " + gameProfile.username
+        return "Granting for " + Chat.format(gameProfile.getCurrentRank().color) + gameProfile.username
     }
 }
