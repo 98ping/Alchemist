c65328bea20a97e9303ee4acc6bf9e4388159c98
98ping <67809373+98ping@users.noreply.github.com>
2023-09-14 17:20:10 -0700

    chore: tried to make disguise then quit

--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/commands/disguise/RankDisguiseCommand.kt
+++ spigot/src/main/kotlin/ltd/matrixstudios/alchemist/commands/disguise/RankDisguiseCommand.kt
@@ -4,9 +4,9 @@ import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import ltd.matrixstudios.alchemist.commands.disguise.menu.RankDisguiseMenu
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.*
-import java.util.concurrent.CompletableFuture
 
 
 class RankDisguiseCommand : BaseCommand() {
