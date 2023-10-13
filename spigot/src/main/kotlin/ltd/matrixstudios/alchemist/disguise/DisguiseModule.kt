2b041102912e7a16bcf9ba340186bf588e0df28b
98ping <67809373+98ping@users.noreply.github.com>
2023-10-12 21:27:45 -0700

    feat: ensure that if disguise provider is null we dont enable the plugin

--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/disguise/DisguiseModule.kt
+++ spigot/src/main/kotlin/ltd/matrixstudios/alchemist/disguise/DisguiseModule.kt
@@ -5,12 +5,19 @@ import ltd.matrixstudios.alchemist.AlchemistSpigotPlugin
 import ltd.matrixstudios.alchemist.disguise.commands.DisguiseCacheCommands
 import ltd.matrixstudios.alchemist.disguise.commands.DisguiseCommand
 import ltd.matrixstudios.alchemist.module.PluginModule
+import net.pinger.disguise.DisguiseAPI
 import org.bukkit.Bukkit
 
 object DisguiseModule : PluginModule
 {
     override fun onLoad()
     {
+        if (DisguiseAPI.getDefaultProvider() == null) {
+            Bukkit.getLogger().info("Failed to find the disguise API provider for this version!");
+            Bukkit.getLogger().info("We are not going to enable the disguise module.");
+            return
+        }
+
         DisguiseService.loadAllSkins()
     }
 
