9b79c6c6c8d57f22414b822ff9f59f032b0d520c
98ping <67809373+98ping@users.noreply.github.com>
2023-12-21 15:15:24 -0800

    feat: forgot to use OnlinePlayer for /cpto and /cpfrom

--- spigot/src/main/kotlin/ltd/matrixstudios/alchemist/essentials/commands/InventoryCopyingCommands.kt
+++ spigot/src/main/kotlin/ltd/matrixstudios/alchemist/essentials/commands/InventoryCopyingCommands.kt
@@ -4,6 +4,7 @@ import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
 import co.aikar.commands.annotation.Name
+import co.aikar.commands.bukkit.contexts.OnlinePlayer
 import ltd.matrixstudios.alchemist.util.Chat
 import org.bukkit.entity.Player
 
@@ -12,8 +13,10 @@ class InventoryCopyingCommands : BaseCommand()
 
     @CommandAlias("cpinv|cpfrom")
     @CommandPermission("alchemist.essentials.cpinv")
-    fun cpInv(sender: Player, @Name("target") player: Player)
+    fun cpInv(sender: Player, @Name("target") online: OnlinePlayer)
     {
+        val player = online.player
+
         sender.inventory.contents = player.inventory.contents
         sender.inventory.armorContents = player.inventory.armorContents
         sender.health = player.health
@@ -25,8 +28,10 @@ class InventoryCopyingCommands : BaseCommand()
 
     @CommandAlias("cpto")
     @CommandPermission("alchemist.essentials.cpto")
-    fun cpTo(sender: Player, @Name("target") player: Player)
+    fun cpTo(sender: Player, @Name("target") online: OnlinePlayer)
     {
+        val player = online.player
+
         player.inventory.contents = sender.inventory.contents
         player.inventory.armorContents = sender.inventory.armorContents
         player.health = sender.health
