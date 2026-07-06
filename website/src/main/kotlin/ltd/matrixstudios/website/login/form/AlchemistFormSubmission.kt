bef1304750114ab8826e5bf8d4212ec91c04788c
Max <67809373+98ping@users.noreply.github.com>
2026-07-05 18:17:10 -0700

    fix: major bump fixes

--- website/src/main/kotlin/ltd/matrixstudios/website/login/form/AlchemistFormSubmission.kt
+++
@@ -1,7 +0,0 @@
-package ltd.matrixstudios.website.login.form
-
-data class AlchemistFormSubmission(
-    val username: String = "Guest",
-    val password: String? = null,
-    val secret: String? = null
-)
